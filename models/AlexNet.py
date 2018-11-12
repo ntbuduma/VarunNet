e2cbef05d61083954d7ee9fe12283be5770d70bb
Seri Choi <serichoi@mit.edu>
2018-11-12 14:29:19 -0500

    added dropouts to AlexNet

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -6,33 +6,38 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes=1000):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
+            nn.Dropout(p=0.2),
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
 
+            nn.Dropout(p=0.2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
 
+            nn.Dropout(p=0.2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.BatchNorm2d(384),
             nn.ReLU(inplace=True),
 
+            nn.Dropout(p=0.2),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
 
+            nn.Dropout(p=0.2),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
         self.classifier = nn.Sequential(
-            nn.Dropout(),
+            nn.Dropout(p=0.2),
             nn.Linear(256 * 7 * 7, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.Dropout(p=0.2),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
